a46a071fb502f152ba6113e9494be64cc99a99dd
int128 <int128@gmail.com>
2016-07-11 18:23:28 +0900

    Refactor groovlet structure

--- src/main/web-inf/routes.groovy
+++ src/main/web-inf/routes.groovy
@@ -3,6 +3,6 @@ post '/authorize', forward: '/auth/exchange-token.groovy'
 post '/webhook', forward: '/github-webhook/receive-event.groovy'
 post '/webhook_test', forward: '/github-webhook/receive-test.groovy'
 
-get '/@owner/@repo/status', forward: '/status.groovy?full_name=@owner/@repo'
+get '/@owner/@repo/status', forward: '/github-repository/status.groovy?full_name=@owner/@repo'
 
-get '/@owner/@repo/status.svg', forward: '/badge/gradle-status.groovy?full_name=@owner/@repo'
+get '/@owner/@repo/status.svg', forward: '/github-repository/badge/gradle-status.groovy?full_name=@owner/@repo'
